c8f19fd70fac8a15042f07517801d740950b51b5
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2023-09-19 15:21:51 -0300

    initial commit

---
+++ javaApi-springRailway/src/test/java/estudo/apirest/spring_railway/SpringRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.apirest.spring_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

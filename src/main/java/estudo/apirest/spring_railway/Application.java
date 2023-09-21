c1112b59f70e59d5ebda187339a3229a77b83ba0
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2023-09-21 15:00:29 -0300

    deploy test

--- src/main/java/estudo/apirest/spring_railway/Application.java
+++ src/main/java/estudo/apirest/spring_railway/Application.java
@@ -3,6 +3,9 @@ package estudo.apirest.spring_railway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @OpenAPIDefinition(servers = {@Server  (url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
